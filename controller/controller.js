23e972141a06beb30ec2e31dc63d5f6a45487b66
fakhriddin01 <a.fakhriddin@gmail.com>
2023-01-26 14:04:15 +0500

    added elon_berish  page, handle inputs from elon berish

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,7 @@
 const {read_file, write_file} = require('../fs/fs_api');
+const {uuid} = require('uuidv4');
+const fs = require('fs');
+
 
 const Controller = {
     OPEN_LOGIN: (req, res)=>{
@@ -11,12 +14,38 @@ const Controller = {
             title: "E'lon berish"
         })
     }, 
-    SENT_ELON: (req, res) =>{
+    SENT_ELON: async(req, res) =>{
+
+        console.log(req.files.image.data);
+        var image = req.files.image.data;
+        let time = new Date()
+
+        let fileName = (time.getMinutes()+time.getSeconds()+req.body.fullname).trim();
+        let ext = (req.files.image.name).split('.').at(-1)
+        console.log(fileName+'.'+ext);
+        fs.writeFile(`uploads/${fileName}.${ext}`, image, function(err){
+            if (err) throw err;
+        });
+       
         let elon = req.body
         let elonlar = read_file('elonlar.json');
         let yunalishlar = ["Information Technologies", "Grafik Dizayn", "SMM"]
         let sub_yunalishlar = [["Node.js", "Python", "Flutter"], ["Grafik pro", "Adobe illustrator", "Photoshop"], ["SMM","Marketing"]]
-        
+        yunalish = yunalishlar[elon.yunalish]
+        ichki_yunalish = sub_yunalishlar[elon.yunalish][elon.ichki_yunalish[elon.yunalish]]
+        elonlar.push({
+            id: uuid(),
+            date: elon.date,
+            time: elon.time,
+            yunalish: yunalish,
+            ichki_yunalish: ichki_yunalish,
+            format: elon.optradio,
+            fullname: elon.fullname,
+            phone: elon.phone,
+            image: fileName+'.'+ext
+        })
+        await write_file('elonlar.json', elonlar);
+        res.redirect('login')
     }
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const exphbs = require('express-handlebars')
 const router = require('./router/router')
+const fileUpload = require('express-fileupload');
 
 const hbs = exphbs.create({
     defaultLayout: "main",
@@ -12,15 +13,14 @@ dotenv.config();
 let port = process.env.PORT;
 
 const app = express();
+app.use(fileUpload());
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
-app.use(express.urlencoded({extended: true}))
-
-app.use(express.json())
 app.use(router)
-
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.listen(port, ()=>{
     console.log(port);
